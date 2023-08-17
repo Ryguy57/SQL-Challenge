c521147de18b77c74a3b52e0e5a26569e0bd266c
Ryguy57 <rdhimes57@gmail.com>
2023-08-17 08:52:10 -0600

    SQL

---
+++ EmployeeSQL/EmployeeSchemas.sql
@@ -0,0 +1,49 @@
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(5)   NOT NULL,
+    "title" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+    )
+);
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+    "title_id" VARCHAR(5)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(20)   NOT NULL,
+    "last_name" VARCHAR(20)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+    ),
+    CONSTRAINT "fk_employees_title_id" FOREIGN KEY ("title_id") REFERENCES "titles" ("title_id")
+);
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    FOREIGN KEY ("emp_no") REFERENCES "employees" ("emp_no")
+);
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(5)   NOT NULL,
+    "dept_name" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+    )
+);
+
+
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(5)   NOT NULL,
+    FOREIGN KEY ("emp_no") REFERENCES "employees" ("emp_no"),
+    FOREIGN KEY ("dept_no") REFERENCES "departments" ("dept_no")
+);
+
+CREATE TABLE "dept_manager" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(5)   NOT NULL,
+    FOREIGN KEY ("emp_no") REFERENCES "employees" ("emp_no"),
+    FOREIGN KEY ("dept_no") REFERENCES "departments" ("dept_no")
+);
